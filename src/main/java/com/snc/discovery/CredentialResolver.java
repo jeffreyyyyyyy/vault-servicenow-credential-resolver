a582fddb6129a2bdf96cc0953f13b3365a8be9da
Tom Proctor <tomhjp@users.noreply.github.com>
2021-07-06 10:44:35 +0100

    Implement core resolve logic (#1)
    * Working credential resolver
    
    * Config + starting tests
    
    * Tidy value extraction, more tests, .editorconfig
    
    * Switch from java.net.http.HTTPClient to Apache HTTP client, add validation
    
    * Add validation tests
    
    * Add back getVersion

--- src/main/java/com/snc/discovery/CredentialResolver.java
+++ src/main/java/com/snc/discovery/CredentialResolver.java
@@ -1,137 +1,254 @@
 package com.snc.discovery;
 
-import java.util.*;
-import java.io.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
 
-/**
- * Basic implementation of a CredentialResolver that uses a properties file.
- */
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import com.service_now.mid.services.Config;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 
 public class CredentialResolver {
-
-    private static String ENV_VAR = "CREDENTIAL_RESOLVER_FILE";
-    private static String DEFAULT_PROP_FILE_PATH = "C:\\dummycredentials.properties";
-
-    // These are the permissible names of arguments passed INTO the resolve()
-    // method.
-
-    // the string identifier as configured on the ServiceNow instance...
-    public static final String ARG_ID = "id";
-
-    // a dotted-form string IPv4 address (like "10.22.231.12") of the target
-    // system...
-    public static final String ARG_IP = "ip";
-
-    // the string type (ssh, snmp, etc.) of credential as configured on the
-    // instance...
-    public static final String ARG_TYPE = "type";
-
-    // the string MID server making the request, as configured on the
-    // instance...
-    public static final String ARG_MID = "mid";
-
-    // These are the permissible names of values returned FROM the resolve()
-    // method.
-
-    // the string user name for the credential, if needed...
-    public static final String VAL_USER = "user";
-
-    // the string password for the credential, if needed...
-    public static final String VAL_PSWD = "pswd";
-
-    // the string pass phrase for the credential if needed:
-    public static final String VAL_PASSPHRASE = "passphrase";
-
-    // the string private key for the credential, if needed...
-    public static final String VAL_PKEY = "pkey";
-
-    // the string authentication protocol for the credential, if needed...
-    public static final String VAL_AUTHPROTO = "authprotocol";
-
-    // the string authentication key for the credential, if needed...
-    public static final String VAL_AUTHKEY = "authkey";
-
-    // the string privacy protocol for the credential, if needed...
-    public static final String VAL_PRIVPROTO = "privprotocol";
-
-    // the string privacy key for the credential, if needed...
-    public static final String VAL_PRIVKEY = "privkey";
-
-
-    private Properties fProps;
+    private final Function<String, String> getProperty;
 
     public CredentialResolver() {
+        getProperty = prop -> Config.get().getProperty(prop);
     }
 
-//    private void loadProps() {
-//        if(fProps == null)
-//            fProps = new Properties();
-//
-//        try {
-//            String propFilePath = System.getenv(ENV_VAR);
-//            if(propFilePath == null) {
-//                System.err.println("Environment var "+ENV_VAR+" not found. Using default file: "+DEFAULT_PROP_FILE_PATH);
-//                propFilePath = DEFAULT_PROP_FILE_PATH;
-//            }
-//
-//            File propFile = new File(propFilePath);
-//            if(!propFile.exists() || !propFile.canRead()) {
-//                System.err.println("Can't open "+propFile.getAbsolutePath());
-//            }
-//            else {
-//                InputStream propsIn = new FileInputStream(propFile);
-//                fProps.load(propsIn);
-//            }
-//            //fProps.load(CredentialResolver.class.getClassLoader().getResourceAsStream("dummycredentials.properties"));
-//        } catch (IOException e) {
-//            System.err.println("Problem loading credentials file:");
-//            e.printStackTrace();
-//        }
-//    }
+    public CredentialResolver(Function<String, String> getProperty) {
+        this.getProperty = getProperty;
+    }
+
+    // Populated keys on resolve's input `Map args`
+    public static final String ARG_ID = "id"; // the string identifier as configured on the ServiceNow instance
+    public static final String ARG_IP = "ip"; // a dotted-form string IPv4 address (like "10.22.231.12") of the target system
+    public static final String ARG_TYPE = "type"; // the string type (ssh, snmp, etc.) of credential
+    public static final String ARG_MID = "mid"; // the MID server making the request
+
+    // Keys that may optionally be populated on resolve's output Map
+    public static final String VAL_USER = "user"; // the string user name for the credential
+    public static final String VAL_PSWD = "pswd"; // the string password for the credential
+    public static final String VAL_PASSPHRASE = "passphrase"; // the string pass phrase for the credential
+    public static final String VAL_PKEY = "pkey"; // the string private key for the credential
 
     /**
      * Resolve a credential.
      */
-    public Map resolve(Map args) {
-        //loadProps();
+    public Map resolve(Map args) throws IOException {
+        String vaultAddress = getProperty.apply("mid.external_credentials.vault.address");
         String id = (String) args.get(ARG_ID);
-        String type = (String) args.get(ARG_TYPE);
-        //String keyPrefix = id+"."+type+".";
 
-        if(id.equalsIgnoreCase("misbehave"))
-            throw new RuntimeException("I've been a baaaaaaaaad CredentialResolver!");
+        String body;
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet get = new HttpGet(vaultAddress + "/v1/" + id);
+            get.setHeader("accept", "application/json");
+            get.setHeader("X-Vault-Request", "true");
+            try (CloseableHttpResponse response = httpClient.execute(get)) {
+                Scanner s = new Scanner(response.getEntity().getContent()).useDelimiter("\\A");
+                body = s.hasNext() ? s.next() : "";
 
-        // the resolved credential is returned in a HashMap...
-        Map result = new HashMap();
-        result.put(VAL_USER, "ssh-user");
-        //result.put(VAL_PSWD, fProps.get(keyPrefix + VAL_PSWD));
-        result.put(VAL_PKEY, "FOO");
-        //result.put(VAL_PASSPHRASE, fProps.get(keyPrefix + VAL_PASSPHRASE));
-        //result.put(VAL_AUTHPROTO, fProps.get(keyPrefix + VAL_AUTHPROTO));
-        //result.put(VAL_AUTHKEY, fProps.get(keyPrefix + VAL_AUTHKEY));
-        //result.put(VAL_PRIVPROTO, fProps.get(keyPrefix + VAL_PRIVPROTO));
-        //result.put(VAL_PRIVKEY, fProps.get(keyPrefix + VAL_PRIVKEY));
+                int status = response.getStatusLine().getStatusCode();
+                if (status < 200 || status > 299) {
+                    String message = String.format("Failed to query Vault for credential id: %s. Status code %d.", id, status);
+                    Gson gson = new Gson();
+                    VaultError error = gson.fromJson(body, VaultError.class);
+                    if (error != null) {
+                        message += " Errors: " + Arrays.toString(error.errors);
+                    }
 
-        System.err.println("Did some phony resolving for credential id/type["+id+"/"+type+"]");
+                    throw new RuntimeException(message);
+                }
+            }
+        }
 
+        System.err.println("Successfully queried Vault for credential id: "+id);
+
+        Map<String, String> result = extractKeys(body);
+        CredentialType type = lookupByName((String) args.get(ARG_TYPE));
+        validateResult(result, type);
         return result;
     }
 
-
     /**
-     * Return the API version supported by this class.
+     * Return the ServiceNow API version supported by this class.
      */
     public String getVersion() {
         return "1.0";
     }
 
-    public static void main(String[] args) {
-        CredentialResolver obj = new CredentialResolver();
-        //obj.loadProps();
+    private Map<String, String> extractKeys(String vaultResponse) {
+        Gson gson = new Gson();
+        VaultSecret secret = gson.fromJson(vaultResponse, VaultSecret.class);
+        JsonObject data = secret.getData();
 
-        System.err.println("I spy the following credentials: ");
-        for(Object key: obj.fProps.keySet()) {
-            System.err.println(key+": "+obj.fProps.get(key));
+        if (data == null) {
+            throw new RuntimeException("No data found in Vault secret");
+        }
+
+        // Check for embedded "data" object to handle kv-v2.
+        if (data.has("data")) {
+            try {
+                data = data.get("data").getAsJsonObject();
+            } catch (IllegalStateException e) {
+                // If it's not a JsonObject, then it's not kv-v2 and we use the top-level "Data" field.
+            }
+        }
+
+        // access_key for AWS secret engine
+        ValueAndSource username = valueAndSourceFromData(data, "access_key", "username");
+        // secret_key for AWS secret engine, current_password for AD secret engine
+        ValueAndSource password = valueAndSourceFromData(data, "secret_key", "current_password", "password");
+        ValueAndSource privateKey = valueAndSourceFromData(data, "private_key");
+        ValueAndSource passphrase = valueAndSourceFromData(data, "passphrase");
+
+        System.err.printf("Setting values from fields %s=%s, %s=%s, %s=%s, %s=%s%n",
+                VAL_USER, username.source,
+                VAL_PSWD, password.source,
+                VAL_PKEY, privateKey.source,
+                VAL_PASSPHRASE, passphrase.source);
+        HashMap<String, String> result = new HashMap<>();
+        if (username.value != null) {
+            result.put(VAL_USER, username.value);
+        }
+        if (password.value != null) {
+            result.put(VAL_PSWD, password.value);
+        }
+        if (privateKey.value != null) {
+            result.put(VAL_PKEY, privateKey.value);
+        }
+        if (passphrase.value != null) {
+            result.put(VAL_PASSPHRASE, passphrase.value);
+        }
+
+        return result;
+    }
+
+    public void validateResult(Map<String, String> result, CredentialType type) {
+        if (result.size() == 0) {
+            throw new RuntimeException("No fields to extract from Vault secret");
+        }
+
+        if (type == null) {
+            return;
+        }
+
+        for (String expected : type.expectedFields()) {
+            if (!result.containsKey(expected)) {
+                throw new RuntimeException(String.format("Expected '%s' field for credential type %s", expected, type.name()));
+            }
+        }
+    }
+
+    private static final Map<String, CredentialType> nameIndex = new HashMap<>(CredentialType.values().length);
+    static {
+        for (CredentialType type : CredentialType.values()) {
+            nameIndex.put(type.name(), type);
+        }
+    }
+    private static CredentialType lookupByName(String name) {
+        return nameIndex.get(name);
+    }
+
+    enum CredentialType {
+        basic                               (new String[]{VAL_USER, VAL_PSWD}),
+        windows                             (new String[]{VAL_USER, VAL_PSWD}),
+        ssh_password                        (new String[]{VAL_USER, VAL_PSWD}),
+        vmware                              (new String[]{VAL_USER, VAL_PSWD}),
+        jdbc                                (new String[]{VAL_USER, VAL_PSWD}),
+        jms                                 (new String[]{VAL_USER, VAL_PSWD}),
+        aws                                 (new String[]{VAL_USER, VAL_PSWD}),
+        ssh_private_key                     (new String[]{VAL_USER, VAL_PKEY}),
+        sn_cfg_ansible                      (new String[]{VAL_USER, VAL_PKEY}),
+        sn_disco_certmgmt_certificate_ca    (new String[]{VAL_USER, VAL_PKEY}),
+        cfg_chef_credentials                (new String[]{VAL_USER, VAL_PKEY}),
+        infoblox                            (new String[]{VAL_USER, VAL_PKEY}),
+        api_key                             (new String[]{VAL_USER, VAL_PKEY});
+
+        private final String[] expectedFields;
+
+        CredentialType(String[] expectedFields) {
+            this.expectedFields = expectedFields;
+        }
+
+        public String[] expectedFields() {
+            return expectedFields;
+        }
+    }
+
+    // Metadata class to help report which fields keys were extracted from.
+    private static class ValueAndSource {
+        private final String value;
+        private final String source;
+
+        ValueAndSource(String value, String source) {
+            this.value = value;
+            this.source = source;
+        }
+    }
+
+    // The first key that exists in data will be extracted and returned.
+    private ValueAndSource valueAndSourceFromData(JsonObject data, String ...keys) {
+        for (String key : keys) {
+            if (data.has(key)) {
+                return new ValueAndSource(data.get(key).getAsString(), key);
+            }
+        }
+
+        return new ValueAndSource(null, null);
+    }
+
+    private static class VaultSecret {
+        private String requestID;
+        private String leaseID;
+        private Integer leaseDuration;
+        private Boolean renewable;
+        private JsonObject data;
+        private String[] warnings;
+        // Auth omitted
+        // WrapInfo omitted
+
+        public String getRequestID() {
+            return requestID;
+        }
+
+        public String getLeaseID() {
+            return leaseID;
+        }
+
+        public Integer getLeaseDuration() {
+            return leaseDuration;
+        }
+
+        public Boolean getRenewable() {
+            return renewable;
+        }
+
+        public JsonObject getData() {
+            return data;
+        }
+
+        public String[] getWarnings() {
+            return warnings;
+        }
+
+        VaultSecret() {
+        }
+    }
+
+    private static class VaultError {
+        private String[] errors;
+
+        public String[] getErrors() {
+            return errors;
         }
     }
 }
--- src/test/java/com/snc/discovery/CredentialResolverTest.java
+++ src/test/java/com/snc/discovery/CredentialResolverTest.java
@@ -1,10 +1,142 @@
 package com.snc.discovery;
 
-import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 
 public class CredentialResolverTest {
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule();
+
+    private Map setupAndResolve(String path, String json) throws IOException {
+        stubFor(get("/v1/" + path)
+            .withHeader("accept", containing("application/json"))
+            .willReturn(ok()
+                .withHeader("Content-Type", "application/json")
+                .withBody(json)));
+
+        var cr = new CredentialResolver(prop -> "http://localhost:8080");
+        var input = new HashMap<String, String>();
+        input.put(CredentialResolver.ARG_ID, path);
+        return cr.resolve(input);
+    }
+
     @Test
-    public void constructorTest() {
-        CredentialResolver testMain = new CredentialResolver();
+    public void testDegenerateCase() {
+        stubFor(get("/v1/degenerate")
+            .withHeader("accept", containing("application/json"))
+            .willReturn(ok()
+                .withHeader("Content-Type", "application/json")
+                .withBody("{}")));
+
+        var cr = new CredentialResolver(prop -> "http://localhost:8080");
+        var input = new HashMap<String, String>();
+        input.put(CredentialResolver.ARG_ID, "degenerate");
+
+        var exception = Assert.assertThrows(RuntimeException.class, () -> cr.resolve(input));
+        Assert.assertTrue(exception.getMessage().contains("No data found"));
+    }
+
+    @Test
+    public void testResolveKvV2() throws IOException {
+        var result = setupAndResolve("secret/data/ssh", "{'data':{'data':{'username':'ssh-user','private_key':'my_very_private_key'}}}");
+
+        Assert.assertEquals("ssh-user", result.get(CredentialResolver.VAL_USER));
+        Assert.assertEquals("my_very_private_key", result.get(CredentialResolver.VAL_PKEY));
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testResolveBasic() throws IOException {
+        var result = setupAndResolve("kv/user", "{'data':{'username':'my-user','password':'my-password'}}");
+
+        Assert.assertEquals("my-user", result.get(CredentialResolver.VAL_USER));
+        Assert.assertEquals("my-password", result.get(CredentialResolver.VAL_PSWD));
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testResolveSshWithPasswordAndPassphrase() throws IOException {
+        var result = setupAndResolve("kv/ssh-with-passphrase", "{'data':{'username':'ssh-user','password':'ssh-password','private_key':'ssh-private-key','passphrase':'ssh-passphrase'}}");
+
+        Assert.assertEquals("ssh-user", result.get(CredentialResolver.VAL_USER));
+        Assert.assertEquals("ssh-password", result.get(CredentialResolver.VAL_PSWD));
+        Assert.assertEquals("ssh-private-key", result.get(CredentialResolver.VAL_PKEY));
+        Assert.assertEquals("ssh-passphrase", result.get(CredentialResolver.VAL_PASSPHRASE));
+        Assert.assertEquals(4, result.size());
+    }
+
+    @Test
+    public void testResolveActiveDirectoryFields() throws IOException {
+        var result = setupAndResolve("ad/ad-user", "{'data':{'username':'my-user','password':'my-password','current_password':'my-current-password'}}");
+
+        Assert.assertEquals("my-user", result.get(CredentialResolver.VAL_USER));
+        Assert.assertEquals("my-current-password", result.get(CredentialResolver.VAL_PSWD));
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testResolveAwsFields() throws IOException {
+        var result = setupAndResolve("aws/aws-user", "{'data':{'username':'aws-user','password':'aws-password','current_password':'aws-current-password','access_key':'aws-access-key','secret_key':'aws-secret-key'}}");
+
+        Assert.assertEquals("aws-access-key", result.get(CredentialResolver.VAL_USER));
+        Assert.assertEquals("aws-secret-key", result.get(CredentialResolver.VAL_PSWD));
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testValidateResultFullyPopulated() {
+        var cr = new CredentialResolver(prop -> "");
+        var input = new HashMap<String, String>();
+        input.put(CredentialResolver.VAL_USER, "");
+        input.put(CredentialResolver.VAL_PSWD, "");
+        input.put(CredentialResolver.VAL_PKEY, "");
+        input.put(CredentialResolver.VAL_PASSPHRASE, "");
+        for (var type : CredentialResolver.CredentialType.values()) {
+            // No validation errors expected
+            cr.validateResult(input, type);
+        }
+
+        cr.validateResult(input, null);
+    }
+
+    @Test
+    public void testValidateResultEmpty() {
+        var cr = new CredentialResolver(prop -> "");
+        var input = new HashMap<String, String>();
+        for (var type : CredentialResolver.CredentialType.values()) {
+            // All types should error for empty input
+            Assert.assertThrows(RuntimeException.class, () -> cr.validateResult(input, type));
+        }
+
+        // null type only validates non-empty
+        Assert.assertThrows(RuntimeException.class, () -> cr.validateResult(input, null));
+        input.put("FOO_KEY", "");
+        // Now that there is some data in the input, no further validation should take place.
+        cr.validateResult(input, null);
+    }
+
+    @Test
+    public void testValidateResultMinimallyPopulated() {
+        var cr = new CredentialResolver(prop -> "");
+        for (var type : CredentialResolver.CredentialType.values()) {
+            var input = new HashMap<String, String>();
+            for (var expected : type.expectedFields()) {
+                input.put(expected, "");
+            }
+            // No validation errors expected
+            cr.validateResult(input, type);
+        }
     }
 }
